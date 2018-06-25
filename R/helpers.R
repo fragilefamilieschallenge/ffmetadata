7ec2f3a78992dca01f7891165d9715995b91dd68
Ryan Vinh <ryanvinh@Ryans-MacBook-Pro.local>
2018-06-24 22:38:37 -0400

    added simplifyDataFrame parameter to fromJSON call

--- R/helpers.R
+++ R/helpers.R
@@ -20,7 +20,9 @@ call_api <- function(endpoint, params) {
     stop("API did not return json", call. = FALSE)
   }
   # read JSON
-  metadata <- jsonlite::fromJSON(httr::content(resp, "text", encoding = "UTF-8"), simplifyVector = FALSE)
+  metadata <- jsonlite::fromJSON(httr::content(resp, "text", encoding = "UTF-8"),
+                                 simplifyVector = FALSE, simplifyDataFrame = TRUE)
+
   # error checking
   if (!is.null(metadata$`error code`)) {
     stop(
