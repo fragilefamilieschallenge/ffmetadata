0d5237a589209875063dba7857db59892ec14c67
Ryan Vinh <ryanvinh@Ryans-MacBook-Pro.local>
2018-07-30 21:27:03 -0400

    added print statements to vignette

--- vignettes/APIUsage.R
+++ vignettes/APIUsage.R
@@ -6,13 +6,16 @@ select_type <- select_metadata(variable_name = "ce3datey", fields = "data_source
 print(select_type)
 
 ## ------------------------------------------------------------------------
-select <- select_metadata(variable_name = "ce3datey", fields = c("data_source", "data_type"))
+select_multiple_fields <- select_metadata(variable_name = "ce3datey", fields = c("data_source", "data_type"))
+print(select_multiple_fields)
 
 ## ------------------------------------------------------------------------
 select_full <- select_metadata(variable_name = "ce3datey")
+print(select_full)
 
 ## ------------------------------------------------------------------------
 select_return_list <- select_metadata(variable_name = "ce3datey", returnDataFrame = FALSE)
+print(select_return_list)
 
 ## ---- results = "asis"---------------------------------------------------
 search <- search_metadata(wave = 3)
