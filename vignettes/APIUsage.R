d3eb500cc6be83d6a21030533b5a230a3e3fe0bf
Ryan Vinh <ryanvinh@Ryans-MacBook-Pro.local>
2018-08-01 00:28:53 -0400

    updated vignette to more console-esque outputs

--- vignettes/APIUsage.R
+++ vignettes/APIUsage.R
@@ -1,34 +1,33 @@
 ## ---- echo=FALSE---------------------------------------------------------
-library(pander)
 library(ffmetadata)
 
 ## ------------------------------------------------------------------------
 select_type <- select_metadata(variable_name = "ce3datey", fields = "data_source")
 
 ## ---- echo=FALSE---------------------------------------------------------
-pander(select_type)
+select_type
 
 ## ---- results='asis'-----------------------------------------------------
 select_multiple_fields <- select_metadata(variable_name = "ce3datey", fields = c("data_source", "data_type"))
 
 ## ---- echo=FALSE---------------------------------------------------------
-pander(select_multiple_fields)
+select_multiple_fields
 
 ## ---- results='asis'-----------------------------------------------------
 select_full <- select_metadata(variable_name = "ce3datey")
 
 ## ---- echo=FALSE---------------------------------------------------------
-  pander(select_full)
+select_full
 
 ## ------------------------------------------------------------------------
 select_return_list <- select_metadata(variable_name = "ce3datey", returnDataFrame = FALSE)
 
 ## ---- echo=FALSE---------------------------------------------------------
-pander(select_return_list)
+select_return_list
 
 ## ---- results = "asis"---------------------------------------------------
-search <- search_metadata(wave = 3)
+search_1 <- search_metadata(wave = "Year 1")
 
 ## ------------------------------------------------------------------------
-search <- search_metadata(wave = 3, data_type = "oc")
+search_1_and <- search_metadata(wave = "Year 1", respondent = "Mother")
 
