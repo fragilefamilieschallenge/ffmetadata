5d4f28348256e085726ef979eae905bc6edae55c
Ryan Vinh <ryanvinh@Ryans-MacBook-Pro.local>
2018-08-25 14:37:37 -0400

    added preliminary tests for select_metadata

---
+++ tests/testthat/test_search_metadata.R
@@ -0,0 +1,4 @@
+library(testthat)
+
+context("Search Metadata")
+
---
+++ tests/testthat/test_select_metadata.R
@@ -0,0 +1,25 @@
+library(testthat)
+
+context("Select Metadata")
+
+test_that("Select Metadata returns a data frame when default parameters used", {
+  default_call <- select_metadata(variable_name = "ce3agefc")
+  expect_that(default_call, is_a("matrix"))
+})
+
+test_that("Select Metadata returns a list when returnDataFrame set to False", {
+  list_return <- select_metadata(variable_name = "ce3agefc", returnDataFrame = FALSE)
+  expect_that(list_return, is_a("list"))
+})
+
+test_that("Select Metadata returns a character type when one field is specified", {
+  one_field <- select_metadata(variable_name = "ce3agefc", fields = "data_type")
+  expect_that(one_field, is_a("character"))
+})
+
+test_that("Select Metadata returns a data frame when multiple fields specified", {
+  multiple_fields <- select_metadata(variable_name = "ce3agefc",
+                                     fields = c("data_type", "data_source"))
+  expect_that(multiple_fields, is_a("matrix"))
+})
+
